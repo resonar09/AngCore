c74cf1f3491b1d71d206394dcb617a19407ba35e
resonar09 <darrenperez@gmail.com>
2019-05-06 17:42:03 -0400

    Full Push

---
+++ AngCore.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using AngCore.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngCore.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>  options) : base (options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
